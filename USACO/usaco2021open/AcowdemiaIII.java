936a60dd02125b7cb2c1c259a74e01c25a5fc98d
Umut Çağın Özcan <umutcaginozcan@gmail.com>
2024-06-06 17:26:45 +0300

    USACO 2021 Open P3

---
+++ USACO/usaco2021open/AcowdemiaIII.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+ 
+public class AcowdemiaIII {
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+        char[][] pasture = new char[n + 2][];
+        pasture[0] = new char[m + 2];
+        Arrays.fill(pasture[0], '.');
+        pasture[n + 1] = pasture[0];
+        for (int y = 1; y <= n; y++) {
+            pasture[y] = ('.' + in.readLine() + '.').toCharArray();
+        }
+        int answer = 0;
+        for (int y = 1; y <= n; y++) {
+            for (int x = 1; x <= m; x++) {
+                if (pasture[y][x] == 'G' && ((pasture[y][x - 1] == 'C' && pasture[y][x + 1] == 'C') || (pasture[y - 1][x] == 'C' && pasture[y + 1][x] == 'C'))) {
+                    pasture[y][x] = '.';
+                    answer++;
+                }
+            }
+        }
+        for (int y = 1; y <= n; y++) {
+            for (int x = 1; x <= m; x++) {
+                if (pasture[y][x] == 'C') {
+                    if (pasture[y + 1][x - 1] == 'C') {
+                        if (pasture[y][x - 1] == 'G') {
+                            pasture[y][x - 1] = '.';
+                            answer++;
+                        } else if (pasture[y + 1][x] == 'G') {
+                            pasture[y + 1][x] = '.';
+                            answer++;
+                        }
+                    }
+                    if (pasture[y + 1][x + 1] == 'C') {
+                        if (pasture[y][x + 1] == 'G') {
+                            pasture[y][x + 1] = '.';
+                            answer++;
+                        } else if (pasture[y + 1][x] == 'G') {
+                            pasture[y + 1][x] = '.';
+                            answer++;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
