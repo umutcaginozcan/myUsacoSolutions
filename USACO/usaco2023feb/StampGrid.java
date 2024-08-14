d353b55ac75714cb6318cfbbce33f8584a4307bb
Umut Çağın Özcan <umutcaginozcan@gmail.com>
2024-08-14 17:11:54 +0300

    fix

--- USACO/usaco2023feb/StampGrid.java
+++ USACO/usaco2023feb/StampGrid.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class StampGrid {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int T = Integer.parseInt(scanner.nextLine());
