2d1af0e95d7cbb946f3c7d433cfa5fba4a73a25b
Umut Çağın Özcan <umutcaginozcan@gmail.com>
2024-11-15 11:35:06 +0300

    '23 Dec P2 (Full Credit!)

--- ContestPrep/23Dec/P2.java
+++ ContestPrep/23Dec/P2.java
@@ -4,8 +4,14 @@ import java.util.Scanner;
 public class P2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+        int N = in.nextInt();  // Read the number of cows
+        in.nextLine();  // Consume the leftover newline character
         int[] cows = new int[N];
+
+        String inp = in.nextLine();  // Read the line of cows' states
+        for (int i = 0; i < N; i++) {
+            cows[i] = inp.charAt(i) - '0';  // Convert each character to an integer ('1' -> 1, '0' -> 0)
+        }
 
         // Compute max possible age of our system. t(max)
         // For that, I need to know which group of 1s limit t(max)
@@ -22,12 +28,15 @@ public class P2 {
         }
         
         int rightmostsize = 0;
+        boolean full1 = false;
         if (cows[cows.length - 1] == 1) {
             for (int i = N - 1; i >= 0; i--) {
                 if (cows[i] == 0)
                     break;
                 if (cows[i] == 1) 
                     rightmostsize++;
+                if (i == 0)
+                    full1 = true;
             }
         }
 
@@ -48,7 +57,25 @@ public class P2 {
                 i = j;
             }
         }
-        sizes.add(rightmostsize);
+        if (!full1) sizes.add(rightmostsize);
 
+        // Find t(max)
+        int tmax = (minsize % 2 == 0) ? (minsize / 2) - 1 :  (minsize / 2);
+        if (leftmostsize != 0) 
+            tmax = Math.min(tmax, leftmostsize - 1);
+        if (rightmostsize != 0) 
+            tmax = Math.min(tmax, rightmostsize - 1);
+
+        // Now we know the max time could have passed since the disease started.
+        // Comptute min possible sick count
+        int res = 0;
+        for (int i = 0; i < sizes.size(); i++) {
+            if (sizes.get(i) % (2 * tmax + 1) == 0) 
+                res += sizes.get(i) / (2 * tmax + 1);
+            else 
+                res += sizes.get(i) / (2 * tmax + 1) + 1;
+        }
+
+        System.out.print(res);
     }
 }
